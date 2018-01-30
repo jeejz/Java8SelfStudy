63e7f31f8cd1c1f265327698e2787c77043f9cdb
jeejz <34750480+jeejz@users.noreply.github.com>
2018-01-30 12:44:50 +0530

    typo correction

--- src/com/java8/challenge/lambda/Solution.java
+++ src/com/java8/challenge/lambda/Solution.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-ublic class Solution {
+public class Solution {
 
  public static void main(String[] args) throws IOException {
   MyMath ob = new MyMath();
